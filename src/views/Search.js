5deb7f06ce53fe8f8aa6505daf7de6d1ad4b61a0
kayyhan <cuvillier.remy@hotmail.fr>
2022-02-22 15:07:55 +0100

    function search

---
+++ src/views/Search.js
@@ -0,0 +1,12 @@
+
+
+function Search(){
+    return(
+        <div className="number_result">
+            <h1>150 résultat de recherche :</h1>
+        </div>
+        <div className="contain">
+
+            </div></>
+    );
+}
