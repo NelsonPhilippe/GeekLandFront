4217e51f2f73b8d98131022831c30a188b8d87a4
kayyhan <cuvillier.remy@hotmail.fr>
2022-02-22 23:31:22 +0100

    css

--- src/views/Search.js
+++ src/views/Search.js
@@ -18,20 +18,11 @@ function Search() {
           <div className="text">
             <h1>Titre de l'objet</h1>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tellus ut rhoncus, porttitor at id odio id
             </p>
-            <div className="price">
-              <h1>50 €</h1>
             </div>
+            <div className="price">
+              <h1>250 €</h1>
           </div>
         </div>
       </div>
