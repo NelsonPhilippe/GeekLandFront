6bdc6ada6bb8804d27000acc670b8efa462c051d
XiliTra <philippenelson59@gmail.com>
2022-02-23 13:59:29 +0100

    add profile menu

--- src/components/ProfileMenu.js
+++ src/components/ProfileMenu.js
@@ -1,10 +1,12 @@
 import * as React from "react";
-
+import './../css/components/ProfileMenu.css'
 
 function MenuProfile(){
     return (
-        <div>
-            
+        <div className="menu-profile" id="menu-profile">
+            <h4>Inscription</h4>
+            <div id="profile-separator"></div>
+            <h4>Connexion</h4>
         </div>
     );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,34 @@
 import * as React from "react";
-import './../css/components/nav.css'
+import './../css/components/Nav.css'
 import { Link } from "react-router-dom";
-import Search from './../components/SearchBar'
+import Search from './SearchBar'
+import Profile from './ProfileMenu'
 
 function Navbar() {
+
+
+    function hoverProfile(status) {
+        let profile = document.getElementById('menu-profile');
+
+        if (status) {
+            profile.style.visibility = 'hidden';
+            return;
+        }
+
+        profile.style.visibility = 'visible';
+
+    }
+
     return (
         <div className='Nav'>
-            <Link id='logo' to='/'><img  src="/img/logo.png" alt="logo" /></Link>
+            <Link id='logo' to='/'><img src="/img/logo.png" alt="logo" /></Link>
             <Search />
             <img id="menu" src='/img/Menu.png' alt='menu_btn' />
-            <img id="profile" src='/img/profile.png' alt='profile_btn' />
+            <img id="profile" src='/img/profile.png' alt='profile_btn'
+            onMouseEnter={() => hoverProfile(false)}
+            onMouseLeave={() => hoverProfile(true)}
+            />
+            <Profile />
         </div>
     );
 }
