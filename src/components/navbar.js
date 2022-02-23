6a4e288ac366a182525aa23c81dde1cac82a6ef5
XiliTra <philippenelson59@gmail.com>
2022-02-23 08:57:51 +0100

    logo

---
+++ src/components/navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class NavBar extends React.Component {
+    render(){
+        return (
+            <div className="nav">
+                <img src="./img/logo.png" alt="logo" />
+            </div>
+        );
+    }
+}
