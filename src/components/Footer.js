31944a189aa9901905a56b93b89c7a7cc006f87d
XiliTra <philippenelson59@gmail.com>
2022-03-01 10:43:26 +0100

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import * as React from "react";
+import "./../css/components/footer.css";
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <div>
+      <div className="footer">
+        <div className="imgfooter">
+          <img src="/img/logo1.png" alt="logo" />
+        </div>
+          <div className="separator1"></div>
+        <div className="linkfooter">
+          <Link to="/">Cookies</Link>
+          <Link to="/">Vos données personnelles</Link>
+          <Link to="/">Coditions générales de vente</Link>
+          <p>Copyright by pornhub</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
