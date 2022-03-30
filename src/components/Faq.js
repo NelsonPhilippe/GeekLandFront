bcdd81fd458ece9864fd56bf60872f72f3068ba9
kayyhan <cuvillier.remy@hotmail.fr>
2022-03-30 21:46:22 +0200

    page terminer / responsiv à faire

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -10,11 +10,53 @@ export default class FAQ extends React.Component {
   render() {
     return (
       <div className="container-FAQ">
-        <h2>Foire Aux Questions</h2>
+        <h1>Foire Aux Questions</h1>
 
         <div className="container-div">
           <div id="articleDef" onClick={this.link}>
-            <p id="articleP">Un article est défectueux</p>
+            <p id="articleP">Un article est défectueux ?</p>
+            <div className="iconArticle">
+              <IoIosArrowForward size={40} color="#2E86C1" />
+            </div>
+          </div>
+
+          <div id="articleDef" onClick={this.link}>
+            <p id="articleP">Un article ne vous convient pas ?</p>
+            <div className="iconArticle">
+              <IoIosArrowForward size={40} color="#2E86C1" />
+            </div>
+          </div>
+
+          <div id="articleDef" onClick={this.link}>
+            <p id="articleP">Vous souhaitez vous faire rembourser ?</p>
+            <div className="iconArticle">
+              <IoIosArrowForward size={40} color="#2E86C1" />
+            </div>
+          </div>
+
+          <div id="articleDef" onClick={this.link}>
+            <p id="articleP">Un problème dans votre commande ?</p>
+            <div className="iconArticle">
+              <IoIosArrowForward size={40} color="#2E86C1" />
+            </div>
+          </div>
+
+          <div id="articleDef" onClick={this.link}>
+            <p id="articleP">Je n’arrive pas à me connecter ?</p>
+            <div className="iconArticle">
+              <IoIosArrowForward size={40} color="#2E86C1" />
+            </div>
+          </div>
+
+          <div id="articleDef" onClick={this.link}>
+            <p id="articleP">Politique de retour</p>
+            <div className="iconArticle">
+              <IoIosArrowForward size={40} color="#2E86C1" />
+            </div>
+          </div>
+
+          <div id="articleDef" onClick={this.link}>
+            <p id="articleP">Autre chose? Contactez nous</p>
             <div className="iconArticle">
               <IoIosArrowForward size={40} color="#2E86C1" />
             </div>
