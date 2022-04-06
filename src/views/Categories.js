f8f2e15b0eec4896230587a5d6cddfc9d2b6152a
MICHAËL CORNIL <michael.cornil@laposte.net>
2022-04-06 10:24:17 +0200

    uncomment gametype

--- src/views/Categories.js
+++ src/views/Categories.js
@@ -1,12 +1,12 @@
 import * as React from 'react';
 // import ProductType from '../components/categories/ProdcutType';
-// import GameType from '../components/categories/GameType';
+import GameType from '../components/categories/GameType';
 
 export default class Categories extends React.Component {
     render() {
         return(
             // <ProductType/>
-            // <GameType/>
+            <GameType/>
         );
     }
 }
