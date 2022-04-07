b4333e0ddbfde79ec284b067ffc5eefed118cfa8
XiliTra <philippenelson59@gmail.com>
2022-04-08 01:40:57 +0200

    fix responsive carousel

--- src/views/Home.js
+++ src/views/Home.js
@@ -6,15 +6,53 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
 class Home extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      carousel_displayed: 10
+    }
+
+    this.updateCarousel = this.updateCarousel.bind(this)
+  }
+
+  componentDidMount(){
+    this.updateCarousel()
+
+    window.addEventListener('resize', this.updateCarousel)
+  }
+  componentWillUnmount(){
+    window.removeEventListener('resize', this.updateCarousel)
+  }
+
+
+  updateCarousel(){
+    let home = document.querySelector('.Home')
+
+    console.log('test');
+
+    if(parseInt(getComputedStyle(home).width) <= 1000){
+
+      return this.setState({
+        carousel_displayed: 5
+      })
+    }
+
+    return this.setState({
+      carousel_displayed: 10
+    })
+  }
+
   render() {
     return (
       <div className="Home">
         <Navbar />
         <CarouselHome />
 
-        <CarouselPromo title="Promotion" display={10} />
+        <CarouselPromo title="Promotion" display={this.state.carousel_displayed} />
 
-        <CarouselForYou />
+
+        <CarouselForYou title="Pour vous" display={this.state.carousel_displayed} />
         <Footer />
       </div>
     );
