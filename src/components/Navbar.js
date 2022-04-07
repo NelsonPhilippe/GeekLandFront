bf913839e06eae6cfc933aa01ad7098ad6549173
XiliTra <philippenelson59@gmail.com>
2022-04-07 20:18:06 +0200

    add cat & add link to cat

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,22 +3,29 @@ import './../css/components/Nav.css'
 import { Link } from "react-router-dom";
 import SearchBar from './SearchBar'
 import Profile from './ProfileMenuNC'
+import ProductType from "./categories/ProdcutType";
 
 class Navbar extends React.Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            visibility: false
+            visibility_profile: false,
+            visibility_cat: false
         }
 
         this.profile = this.profile.bind(this)
+        this.categorie = this.categorie.bind(this)
+
     }
 
 
-    profile(e) {
 
-        let visibility = this.state.visibility
+
+
+    profile() {
+
+        let visibility = this.state.visibility_profile
         // let element = document.querySelector('.menu-profile')
 
         
@@ -26,25 +33,48 @@ class Navbar extends React.Component {
         if (visibility) {
             
             return this.setState({
-                visibility: false
+                visibility_profile: false
             })
         }
         this.setState({
-            visibility: true
+            visibility_profile: true
         })
 
     }
+
+
+    categorie() {
+
+        let visibility = this.state.visibility_cat
+        // let element = document.querySelector('.menu-profile')
+
+        
+
+        if (visibility) {
+            
+            return this.setState({
+                visibility_cat: false
+            })
+        }
+        this.setState({
+            visibility_cat: true
+        })
+
+    }
+
 
     render() {
         return (
             <div className='Nav'>
                 <Link id='logo' to='/'><img src="/img/logo.png" alt="logo" /></Link>
                 <SearchBar />
-                <img id="menu" src='/img/Menu.png' alt='menu_btn' />
+                <img id="menu" src='/img/Menu.png' alt='menu_btn' onClick={this.categorie}  />
                 <img id="profile" src='/img/profile.png' alt='profile_btn'
                     onClick={this.profile} 
                 />
-                {this.state.visibility ? <Profile /> : null}
+                {this.state.visibility_profile ? <Profile /> : null}
+                {this.state.visibility_cat ? <ProductType /> : null}
+
             </div>
         );
     }
