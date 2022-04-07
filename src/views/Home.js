dcccdcd3aacae51c7bb4c07a0d696f46ce9daef1
XiliTra <philippenelson59@gmail.com>
2022-04-07 13:00:32 +0200

    add navbar

--- src/views/Home.js
+++ src/views/Home.js
@@ -3,12 +3,16 @@ import CarouselHome from "../components/carousel/CarouselHome";
 import CarouselPromo from "../components/carousel/CarouselPromo"
 import CarouselForYou from "../components/carousel/CarouselForYou"
 import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 
 class Home extends React.Component {
   render() {
     return (
       <div className="Home">
+
+        <Navbar />
+
         <CarouselHome />
 
         <CarouselPromo title="Promotion" display={10} />
