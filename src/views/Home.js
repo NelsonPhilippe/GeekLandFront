fa778b2c775eff03e1bf324d3b52b49073687117
XiliTra <philippenelson59@gmail.com>
2022-04-12 14:58:36 +0200

    add profile menu to home on register & add cgu + fix

--- src/views/Home.js
+++ src/views/Home.js
@@ -4,35 +4,38 @@ import CarouselPromo from "../components/carousel/CarouselPromo"
 import CarouselForYou from "../components/carousel/CarouselForYou"
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import Cgu from './../components/Cgu'
 
 
 class Home extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
-      carousel_displayed: 10
+      carousel_displayed: 10,
+      display_cgu: false
     }
 
     this.updateCarousel = this.updateCarousel.bind(this)
+    this.displayCgu = this.displayCgu.bind(this)
+
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.updateCarousel()
+    // console.log(this.props.isLogin);
 
     window.addEventListener('resize', this.updateCarousel)
   }
-  componentWillUnmount(){
+  componentWillUnmount() {
     window.removeEventListener('resize', this.updateCarousel)
   }
 
 
-  updateCarousel(){
+  updateCarousel() {
     let home = document.querySelector('.Home')
 
-    console.log('test');
-
-    if(parseInt(getComputedStyle(home).width) <= 1000){
+    if (parseInt(getComputedStyle(home).width) <= 1000) {
 
       return this.setState({
         carousel_displayed: 5
@@ -44,9 +47,17 @@ class Home extends React.Component {
     })
   }
 
+  displayCgu() {
+    console.log('test');
+    this.setState({ display_cgu: !this.state.display_cgu })
+
+  }
+
   render() {
     return (
       <div className="Home">
+
+        {this.state.display_cgu ? <Cgu displayCgu={this.displayCgu} /> : null}
 
         <Navbar />
 
@@ -56,7 +67,8 @@ class Home extends React.Component {
 
 
         <CarouselForYou title="Pour vous" display={this.state.carousel_displayed} />
-        <Footer />
+        <Footer displayCgu={this.displayCgu} />
+
       </div>
     );
   }
