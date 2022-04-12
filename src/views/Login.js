984c3573f2d9dfa68261fb4950b26f5a628f8651
XiliTra <philippenelson59@gmail.com>
2022-04-12 17:35:46 +0200

    fix duplicate render in login

--- src/views/Login.js
+++ src/views/Login.js
@@ -111,37 +111,4 @@ export default class Fieldlogin extends React.Component {
 			</div>
 		);
 	}
-    render() {
-        return (
-            <div className="connexion">
-                <div className="fieldlogin">
-                    <form action="" method="get" className="connexion">
-
-
-                        <div className='id'>
-                            <label for="login">Identifiant</label>
-                            <input type="text" id="login" name="login" placeholder="Enter your mail adress" required="true" />
-                            <div className='remember'>
-                                <input type="checkbox" id="check-login" name="login" />
-                                <label id="check-login-label" for="login">Se souvenir de moi</label>
-                            </div>
-                        </div>
-
-
-
-                        <div className="password">
-                            <label for="password">Mot de passe</label>
-                            <input type="password" id="password" name="password" placeholder="Enter your Password" required="true" />
-                            <label id="password-label" for="password">Mot de passe oublié?</label>
-                        </div>
-
-                        <div className="submit">
-                            <button id="submit">Connexion   </button>
-                            <a href="">Créer un nouveau compte</a>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        )
-    }
 }
