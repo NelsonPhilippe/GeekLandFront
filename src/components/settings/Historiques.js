9ec1529fdb5e04e8a6d346eea93c23b2b0f1bc13
Kayyhan <remy@debian-BULLSEYE-live-builder-AMD64>
2022-04-14 14:15:49 +0200

    fin du front Historiques.js

--- src/components/settings/Historiques.js
+++ src/components/settings/Historiques.js
@@ -53,8 +53,40 @@ export default class Historiques extends React.Component {
               {/* Mettre l'image du produit*/}
             </div>
             <div className="article-title">
-              <h4>Titre du produit</h4> {/* Mettre le titre du produit*/}
+              <p>Titre du produit</p> {/* Mettre le titre du produit*/}
               <button className="btn-avis-article">DONNER MON AVIS</button>
+            </div>
+            <p id="article-quantite">x1</p>{" "}
+            {/* Mettre la quantité du produit commandé*/}
+            <p id="article-price">80€</p> {/* Mettre le prix du produit*/}
+          </div>
+          <div className="line-gray"></div>
+          <div className="info-general">
+            <div className="info-user">
+              <div className="mode-livraison">
+                <p id="mode">Mode de livraison :</p>
+                <p id="result-mode">Chronopost express</p>
+              </div>
+              <div className="adress-user">
+                <p>M.Jean Michelle</p>
+                <p>4, rue de la boucherie</p>
+                <p>59140 Dunkerque</p>
+              </div>
+              <div className="mode-paiement">
+                <p id="mode">Mode de paiement :</p>
+                <p id="result-mode">Carte bancaire</p>
+              </div>
+            </div>
+            <div className="price-general">
+              <div className="price-port">
+                <p id="title-port">Frais de port :</p>
+                <p id="price-port">13.95€</p>
+              </div>
+              <div className="price-total">
+                <p id="title-total">Total de la commande :</p>
+                <p id="price-total">90,90€</p>{" "}
+                {/* Mettre le prix total de la commande (frais de port + prix article)*/}
+              </div>
             </div>
           </div>
         </div>
